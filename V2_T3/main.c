3fd0dbd0ecc35797b4cc4a4829ad656177defdd2
JnsRdnp <joonasroska123@gmail.com>
2022-09-03 20:16:56 +0300

    T3

---
+++ V2_T3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Ohjelma kysyy; käyttäjältä viikonpäivän numeron(1-7)
+//Numeron antamisen mukaan ohjelma tulostaa viikonpäivän.
+//Jos käyttäjä antaa muun numeron kuin 1-7, tulostuu käyttäjälle teksti
+//"Annoit sellaisen numeron, jolle ei ole viikonpäivää"
+
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
